6b22ba44f6c97024342aae84d715cf34f3637344
Arman Jalilian <arman.jalilian@krakenflex.com>
2025-11-15 22:54:32 +0000

    improvement: ensure Google maps SDK for android is enabled and the api key to access is created through IaC. Dynamically assign the key through use of local.properties.

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,8 @@ variable "billing_account" {
   description = "The billing account for the project"
   type        = string
 }
+
+variable "app_sha1_fingerprint" {
+  description = "The SHA1 fingerprint of the app"
+  type        = string
+}
