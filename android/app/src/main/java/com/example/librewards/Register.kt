9e31aaa9b91f7ba5d3aec759dfc13f5129102005
Arman Jalilian <arman_jn@hotmail.co.uk>
2025-11-15 15:27:42 +0000

    chore: upgrade Firebase library and Kotlin versions

--- android/app/src/main/java/com/example/librewards/Register.kt
+++ android/app/src/main/java/com/example/librewards/Register.kt
@@ -7,12 +7,12 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.librewards.databinding.ActivityRegisterBinding
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.ktx.Firebase
 
 class Register : AppCompatActivity() {
     private lateinit var localDb: DatabaseHandler
