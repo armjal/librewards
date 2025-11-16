abdf4532d2a74927420104a5724ea435cda25cc6
Arman Jalilian <arman.jalilian@krakenflex.com>
2025-11-16 23:24:14 +0000

    improvement: introduce project metadata and GCP dependencies through pyproject.toml. Add to main.py to suit GCP functions format

--- functions/set-admin-auth/main.py
+++ functions/set-admin-auth/main.py
@@ -1,2 +1,10 @@
-def handler():
-    pass
+import functions_framework
+
+
+@functions_framework.cloud_event
+def hello_world(event):
+    """Cloud Event Function.
+    Args:
+        event: Cloud event for the function trigger
+    """
+    print("Hello, stdout!")
