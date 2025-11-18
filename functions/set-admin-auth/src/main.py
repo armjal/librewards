b01fb4f33b25d4f87694b98d420a500a65086d96
Arman Jalilian <arman.jalilian@krakenflex.com>
2025-11-18 23:27:46 +0000

    feat: set custom admin auth claim for user based on user id. Capture failures and respond/log accordingly

--- functions/set-admin-auth/src/main.py
+++ functions/set-admin-auth/src/main.py
@@ -2,10 +2,16 @@ from http import HTTPStatus
 import json
 from typing import TypedDict
 from werkzeug.wrappers import Request
+from firebase_admin.auth import set_custom_user_claims
+import logging
 
 
 class RequestData(TypedDict):
     user_id: str
+
+
+class CustomAuthClaims(TypedDict):
+    admin: bool
 
 
 def handler(request: Request):
@@ -16,4 +22,16 @@ def handler(request: Request):
         A JSON string with status and user_id
     """
     request_data: RequestData = request.get_json()
-    return json.dumps({"status": HTTPStatus.OK, "data": request_data["user_id"]})
+    claims = CustomAuthClaims(admin=True)
+
+    try:
+        set_custom_user_claims(request_data["user_id"], claims)
+        logging.debug(f"Set admin claims for user_id: {request_data['user_id']}")
+
+        return json.dumps({"status": HTTPStatus.OK, "data": request_data["user_id"]})
+
+    except Exception as e:
+        logging.error(
+            f"Error setting admin claims for user_id: {request_data['user_id']}: {e}"
+        )
+        return json.dumps({"status": HTTPStatus.INTERNAL_SERVER_ERROR, "error": "Failed to set claims"})
--- functions/set-admin-auth/tests/test_main.py
+++ functions/set-admin-auth/tests/test_main.py
@@ -1,11 +1,34 @@
+from unittest.mock import Mock, patch
+
 from main import RequestData, handler
 from werkzeug.wrappers import Request
+from firebase_admin.exceptions import FirebaseError
 
 
-def test_handler():
+@patch("main.set_custom_user_claims")
+def test__handler__given_user_id__successfully_sets_admin_auth_claim(
+    mock_set_custom_user_claims: Mock,
+):
     request_data: RequestData = {"user_id": "test-id"}
     request: Request = Request.from_values(json=request_data)
 
     response = handler(request)
 
+    mock_set_custom_user_claims.assert_called_once_with("test-id", {"admin": True})
     assert response == '{"status": 200, "data": "test-id"}'
+
+
+@patch("main.set_custom_user_claims")
+def test__handler__given_set_claims_failure__raises_exception(
+    mock_set_custom_user_claims: Mock,
+):
+    request_data: RequestData = {"user_id": "test-id"}
+    request: Request = Request.from_values(json=request_data)
+    mock_set_custom_user_claims.side_effect = FirebaseError(
+        code="error-code", message="Failed to set claims"
+    )
+
+    response = handler(request)
+
+    mock_set_custom_user_claims.assert_called_once_with("test-id", {"admin": True})
+    assert response == '{"status": 500, "error": "Failed to set claims"}'
