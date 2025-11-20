18ce95d4452c126b0a6b6867f4bc2c76d18bb3fc
Arman Jalilian <arman.jalilian@krakenflex.com>
2025-11-20 22:51:40 +0000

    chore: create basic skeleton ProductRepository

--- android/app/src/main/java/com/example/librewards/repositories/ProductRepository.kt
+++ android/app/src/main/java/com/example/librewards/repositories/ProductRepository.kt
@@ -1,4 +1,17 @@
 package com.example.librewards.repositories
 
-class ProductRepository {
+import com.example.librewards.clients.Firebase
+import com.example.librewards.models.Product
+
+class ProductRepository(database: Firebase.RealtimeDatabase, storage: Firebase.Storage) {
+    val database = Firebase.RealtimeDatabase()
+    val storage = Firebase.Storage()
+
+
+    fun addProduct(product: Product?) {
+    }
+
+    fun updateProduct(product: Product?) {}
+
+    fun deleteProduct(id: Int) {}
 }
