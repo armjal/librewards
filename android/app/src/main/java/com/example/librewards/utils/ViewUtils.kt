96c8b740c9142f6b37cc2cf8b07b55fb24ea7610
Arman Jalilian <arman.jalilian@krakenflex.com>
2025-11-30 14:48:20 +0000

    refactor: move showPopup to viewutils

--- android/app/src/main/java/com/example/librewards/utils/ViewUtils.kt
+++ android/app/src/main/java/com/example/librewards/utils/ViewUtils.kt
@@ -1,9 +1,24 @@
 package com.example.librewards.utils
 
+import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.core.graphics.drawable.toDrawable
+import com.example.librewards.databinding.PopupLayoutBinding
 
 
 fun toastMessage(context: Context, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+}
+
+fun showPopup(context: Context, text: String?) {
+    val popup = Dialog(context)
+    popup.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+    val popupLayoutBinding = PopupLayoutBinding.inflate(LayoutInflater.from(context))
+    popup.setContentView(popupLayoutBinding.root)
+    popupLayoutBinding.popupText.text = text
+    popupLayoutBinding.closeBtn.setOnClickListener { popup.dismiss() }
+    popup.show()
 }
