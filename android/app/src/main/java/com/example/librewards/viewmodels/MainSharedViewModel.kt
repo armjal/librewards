1d4048e2c704d6325872b432fe06ee0cfb4c74b8
Arman <87311642+armjal@users.noreply.github.com>
2025-12-01 17:36:32 +0000

    improvement: introduce shared view model so that timer and rewards make user changes and listen to points changes (#11)
    * improvement: introduce MainViewModel which is shared across Rewards and Timer Fragments. Initial purpose is to ensure updates to points are in sync without need for bespoke listeners
    
    * refactor: remove redundant listeners
    
    * improvement: ensure fragments are able to update points given timer completion or redeeming rewards
    
    * refactor: renmame view model
    
    ---------
    
    Co-authored-by: Arman Jalilian <arman.jalilian@krakenflex.com>

---
+++ android/app/src/main/java/com/example/librewards/viewmodels/MainSharedViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.librewards.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.librewards.models.User
+import com.example.librewards.repositories.UserRepository
+
+class MainSharedViewModel(val userRepo: UserRepository): ViewModel() {
+    private var _updatedUser = MutableLiveData<User>()
+    val updatedUser: LiveData<User> get() = _updatedUser
+
+    fun updatePoints(points: String){
+        val user = _updatedUser.value
+        user?.points = points
+        userRepo.updateUser(user!!)
+    }
+
+    fun getUser(email: String){
+        userRepo.getUser(email).addOnSuccessListener {
+            _updatedUser.value = it?.getValue(User::class.java)
+        }
+    }
+}
+
+class MainViewModelFactory(
+    private val userRepo: UserRepository,
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(MainSharedViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            MainSharedViewModel(userRepo) as T
+        } else {
+            throw IllegalArgumentException("ViewModel Not Found")
+        }
+    }
+}
