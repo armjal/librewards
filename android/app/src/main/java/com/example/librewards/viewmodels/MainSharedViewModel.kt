5ffbb13f8b81a4caaec0224a9230a80fb0e9c12d
Arman <87311642+armjal@users.noreply.github.com>
2025-12-06 18:57:45 +0000

    improvement: allow ViewModels for Timer to be responsible for updating points, studying, and stopwatch status (#12)
    * improvement: ensure MainActivity is responsible for setting up the User event listener and Login User listener only applies once
    
    * improvement: ensure Timer handling is done within viewmodels including updating points, listening for user changes, and updating studying status.
    - This commit involved a lot of experimentation between implementations hence the size of commit
    
    * refactor: split out large timer listener into separate, clear functions
    
    ---------
    
    Co-authored-by: Arman Jalilian <arman.jalilian@krakenflex.com>

--- android/app/src/main/java/com/example/librewards/viewmodels/MainSharedViewModel.kt
+++ android/app/src/main/java/com/example/librewards/viewmodels/MainSharedViewModel.kt
@@ -1,26 +1,41 @@
 package com.example.librewards.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.example.librewards.models.User
 import com.example.librewards.repositories.UserRepository
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import java.lang.Integer.parseInt
 
-class MainSharedViewModel(val userRepo: UserRepository): ViewModel() {
+class MainSharedViewModel(val userRepo: UserRepository) : ViewModel() {
+    companion object {
+        val TAG: String = MainSharedViewModel::class.java.simpleName
+    }
+
     private var _updatedUser = MutableLiveData<User>()
     val updatedUser: LiveData<User> get() = _updatedUser
 
-    fun updatePoints(points: String){
-        val user = _updatedUser.value
-        user?.points = points
-        userRepo.updateUser(user!!)
+    fun startListeningForUserUpdates(email: String) {
+        userRepo.listenForUserUpdates(email)
+            .onEach { updatedUser ->
+                _updatedUser.value = updatedUser
+
+            }
+            .catch { error ->
+                Log.e(TAG, "Error observing user: ${error.message}")
+            }.launchIn(viewModelScope)
     }
 
-    fun getUser(email: String){
-        userRepo.getUser(email).addOnSuccessListener {
-            _updatedUser.value = it?.getValue(User::class.java)
-        }
+    fun updatePoints(points: Int) {
+        val updatedPoints = parseInt(_updatedUser.value?.points!!) + points
+        _updatedUser.value?.points = updatedPoints.toString()
+        userRepo.updateUser(_updatedUser.value!!)
     }
 }
 
