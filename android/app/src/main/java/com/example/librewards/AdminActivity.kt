5d794cc61f41c3533b919dbd367b630d0b3ceab9
Arman Jalilian <arman_jn@hotmail.co.uk>
2025-12-22 18:40:45 +0000

    refactor(AdminActivity): make functionality concise. Use shared utils functions including a new extension method on ViewPager2 to be used by MainActivity as well

--- android/app/src/main/java/com/example/librewards/AdminActivity.kt
+++ android/app/src/main/java/com/example/librewards/AdminActivity.kt
@@ -1,21 +1,23 @@
 package com.example.librewards
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import com.example.librewards.adapters.ViewPagerAdapter
 import com.example.librewards.databinding.ActivityAdminBinding
 import com.example.librewards.repositories.UserRepository
+import com.example.librewards.utils.setupWithFragments
+import com.example.librewards.utils.startLibRewardsActivity
 import com.example.librewards.viewmodels.AdminViewModel
 import com.example.librewards.viewmodels.AdminViewModelFactory
-import com.google.android.material.tabs.TabLayoutMediator
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.database.FirebaseDatabase
 
 class AdminActivity : AppCompatActivity() {
+    companion object {
+        val TAG: String = AdminActivity::class.java.simpleName
+    }
+
     private val viewModel: AdminViewModel by viewModels {
         val userRepo = UserRepository(FirebaseDatabase.getInstance().reference)
         AdminViewModelFactory(userRepo)
@@ -25,21 +27,22 @@ class AdminActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Sets the layout to the XML file associated with it
         binding = ActivityAdminBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.adminProfileImage.setOnClickListener { logoutApp() }
 
-        val viewPagerAdapter = ViewPagerAdapter(this)
-        binding.adminViewPager.adapter = viewPagerAdapter
-        val fragments = listOf(AdminHomeFragment(), AdminRewardsFragment())
-        viewPagerAdapter.addFragments(fragments)
-        TabLayoutMediator(binding.adminTabLayout, binding.adminViewPager) { tab, position ->
-            tab.icon = ContextCompat.getDrawable(this, fragments[position].icon)
-        }.attach()
-
+        setupButtonListeners()
+        setupFragmentsView()
         viewModel.setUser(Firebase.auth.currentUser?.email!!)
         observeUser()
+    }
+
+    private fun setupFragmentsView() {
+        val fragmentsToCreate = listOf(AdminHomeFragment(), AdminRewardsFragment())
+        binding.adminViewPager.setupWithFragments(this, binding.adminTabLayout, fragmentsToCreate)
+    }
+
+    private fun setupButtonListeners() {
+        binding.adminProfileImage.setOnClickListener { logoutApp() }
     }
 
     private fun observeUser() {
@@ -49,27 +52,11 @@ class AdminActivity : AppCompatActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        for (fragment in supportFragmentManager.fragments) {
-            fragment.onActivityResult(requestCode, resultCode, data)
-        }
-    }
-
     private fun logoutApp() {
         val auth = Firebase.auth
         if (auth.currentUser != null) {
             auth.signOut()
-            val intent = Intent(this, Login::class.java)
-            intent.flags =
-                Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-            supportFragmentManager.popBackStack()
-            startActivity(intent)
-            finish()
+            startLibRewardsActivity(this, Login::class.java)
         }
-    }
-
-    companion object {
-        val TAG: String = AdminActivity::class.java.simpleName
     }
 }
--- android/app/src/main/java/com/example/librewards/utils/ViewUtils.kt
+++ android/app/src/main/java/com/example/librewards/utils/ViewUtils.kt
@@ -7,8 +7,14 @@ import android.content.Intent
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getDrawable
 import androidx.core.graphics.drawable.toDrawable
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.example.librewards.adapters.ViewPagerAdapter
 import com.example.librewards.databinding.PopupLayoutBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 fun toastMessage(context: Context, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_LONG).show()
@@ -28,4 +34,17 @@ fun startLibRewardsActivity(activityContext: Activity, activityToStart: Class<*>
     val intent = Intent(activityContext, activityToStart)
     activityContext.startActivity(intent)
     activityContext.finish()
+}
+
+fun ViewPager2.setupWithFragments(
+    activity: FragmentActivity,
+    tabLayout: TabLayout,
+    fragments: List<FragmentExtended>,
+) {
+    val viewPagerAdapter = ViewPagerAdapter(activity)
+    this.adapter = viewPagerAdapter
+
+    TabLayoutMediator(tabLayout, this) { tab, position ->
+        tab.icon = getDrawable(activity, fragments[position].icon)
+    }.attach()
 }
