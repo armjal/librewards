6609b2dd33cff092f93c5461c6baf3a74893a5bb
Arman Jalilian <arman_jn@hotmail.co.uk>
2025-12-29 19:42:13 +0000

    refactor(integration-test): create local server utils to centralise requests

---
+++ android/app/src/androidTest/java/com/example/librewards/utils/LocalServerUtils.kt
@@ -0,0 +1,23 @@
+package com.example.librewards.utils
+
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
+
+data class ServerResponse(
+    val status: Int,
+    val message: String,
+)
+
+object LocalServerUtils {
+    fun post(url: URL, requestBody: JSONObject? = null): ServerResponse {
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "POST"
+        connection.connectTimeout = 5000
+        connection.readTimeout = 5000
+        if (requestBody != null) connection.outputStream.use { it.write(requestBody.toString().toByteArray()) }
+        val responseCode = connection.responseCode
+
+        return ServerResponse(responseCode, connection.inputStream.bufferedReader().readText())
+    }
+}
