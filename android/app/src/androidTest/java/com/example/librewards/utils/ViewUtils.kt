98ad56cf81b139e3a55385b268a36946a90ca0b9
Arman <87311642+armjal@users.noreply.github.com>
2025-12-29 14:03:18 +0000

    test(integration): add test cases for Registration flow (#24)
    * test(integration): add Login test flows for student
    
    * test(integration): add Registration integration test with initial successful registration case
    
    * test(integration): add Registration integration test with initial successful registration case
    
    * test(integration): add Registration case when password not acceptable for Firebase auth
    
    ---------
    
    Co-authored-by: Arman Jalilian <arman.jalilian@krakenflex.com>

--- android/app/src/androidTest/java/com/example/librewards/utils/ViewUtils.kt
+++ android/app/src/androidTest/java/com/example/librewards/utils/ViewUtils.kt
@@ -6,6 +6,9 @@ import androidx.test.espresso.ViewAction
 import androidx.test.espresso.matcher.ViewMatchers.isClickable
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
+import androidx.test.runner.lifecycle.Stage
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.Matcher
 
@@ -18,6 +21,17 @@ object ViewUtils {
         override fun perform(uiController: UiController, view: View) {
             view.performClick() // perform click without checking view coordinates.
             uiController.loopMainThreadUntilIdle()
+        }
+    }
+
+    fun finishAllActivities() {
+        InstrumentationRegistry.getInstrumentation().runOnMainSync {
+            val activities = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED)
+            for (activity in activities) {
+                if (!activity.isFinishing) {
+                    activity.finish()
+                }
+            }
         }
     }
 }
